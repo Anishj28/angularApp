63f49580812835cad4520833fefcf4dff4efe434
Anishj28 <anishjawaji28@gmail.com>
2021-05-07 16:41:00 +0530

    assignment 20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,5 @@ import {NgForm} from '@angular/forms'
 })
 
 export class AppComponent {
-  users=[];
-  
-  addUser(ref:NgForm){
-
-    let userObj=ref.value;
-    //push users to array
-    this.users.push(userObj);
-    //Clear Form
-    ref.reset();
-}
-  //Delete User
-  deleteUser(index){
-      this.users.splice(index,1);
-  }
+  card=[1,2,3,4,5,6];
 }
